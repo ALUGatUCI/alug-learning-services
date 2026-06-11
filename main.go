4b318e6aa5366f0c1f8986dccdfc92543f1b66ad
robotman40 <robotman40@protonmail.com>
2026-06-11 15:56:18 -0700

    Load a .env file on launch

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 import (
 	"os"
 	"fmt"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables first
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Failed to load environment variables: %s\n", err)
+		os.Exit(1)
+	}
+
 	client, err := NewPodman()
 	if err != nil {
 		fmt.Println(err)
